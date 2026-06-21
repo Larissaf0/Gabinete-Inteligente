41cf033f800b8b8afe303e1d3464162ac4f9b444
Larissa <98364610+Larissaf0@users.noreply.github.com>
2026-06-21 17:25:57 -0300

    versão 0.2 sidebar/aba reunião/hora

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from fastapi import FastAPI, Request, Form, HTTPException,status
 from fastapi.responses import HTMLResponse, RedirectResponse
 import httpx
@@ -5,7 +6,9 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from database import Base, engine
 from datetime import datetime
+import database
 from models import Secretaria
+from schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -18,6 +21,12 @@ RECAPTCHA_SECRET_KEY = "6LfImgQtAAAAALk3enMyZUDRMqRTH3LRD1bO5K-e"
       #  request=request,
      #   name="home.html"
     #)
+
+#landing page, é a tela principal do site, onde faz o login
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(request, "index.html")
+
 @app.post("/login")
 async def login(
     request: Request,
@@ -60,42 +69,120 @@ async def tela_solicitar_acessp(request: Request):
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+#tela HOME, é a página inicial após o login. Com painel lateral e escolha de secretaria.
 @app.get("/home", response_class=HTMLResponse)
 async def home_page(request: Request):
-    usuario = {"nome": "Admin"} # Exemplo de usuário
+    usuario = {"nome": "admin"} # Exemplo de usuário
     return templates.TemplateResponse(request,"home.html", context={"usuario": usuario})
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse(request, "index.html")
+# landing page, é a tela principal do site, onde faz o login
+# @app.get("/", response_class=HTMLResponse)
+# async def home(request: Request):
+#    return templates.TemplateResponse(request, "index.html")
 
-# Página para criar nova reunião, com o nome da secretaria injetado dinamicamente
-@app.get("/nova-reuniao/{secretaria_nome}", response_class=HTMLResponse)
-async def pagina_nova_reuniao(request: Request, secretaria_nome: str):
+#Página home de cada secretaria (dashboard será específico)
+@app.get("/sec-home", response_class=HTMLResponse)
+async def sec_home(request: Request):
+    return templates.TemplateResponse(request,"sec_home.html")
+          
+
+#página padrão de cada reunião - 
+@app.get("/nova-reuniao",response_class=HTMLResponse)
+async def nova_reuniao(request: Request):
 
     data_atual = datetime.now().strftime("%d/%m/%Y")
-    
-    # Mapeamento para deixar o nome com acentuação e espaços corretos na tela
-    nomes_formatados = {
-        "Educacao": "Educação",
-        "Saude": "Saúde",
-        "Administracao": "Administração",
-        "ServicosPublicos":"Serviços Públicos",
-        "DesenvolvimentoEconomico": "Desenvolvimento Econômico, Ciência e Tecnologia",
-    }
-    
-    # Busca o nome limpo no dicionário. Se não achar, usa o próprio texto digitado na URL
-    secretaria_display = nomes_formatados.get(secretaria_nome, secretaria_nome)
-    
-    # Injeta dinamicamente a variável 'secretaria' no contexto do template padrão
+
     return templates.TemplateResponse(
         request=request,
         name="nova_reuniao.html",
-        context={"secretaria": secretaria_display,
-                 "data_atual": data_atual,
-                 "hora_atual": datetime.now().strftime("%H:%M")
+        context={
+            "data_atual": data_atual,
+            "hora_atual": datetime.now().strftime("%H:%M")
         }
     )
+
+# Página para criar nova reunião, com o nome da secretaria injetado dinamicamente
+# @app.get("/nova-reuniao/{{secretaria_nome}}", response_class=HTMLResponse)
+# async def nova_reuniao(request: Request, secretaria_nome: str):
+
+#     data_atual = datetime.now().strftime("%d/%m/%Y")
+    
+#     # Mapeamento para deixar o nome com acentuação e espaços corretos na tela
+#     nomes_formatados = {
+#         "SEDUC": "Educação",
+#         "Financas": "Finanças",
+#         "Saude": "Saúde",
+#         "ADM": "Administração",
+#         "ServicosPublicos":"Serviços Públicos",
+#         "SEDTEC": "Desenvolvimento Econômico, Ciência e Tecnologia",
+#         "CulturaEsportes": "Cultura e Esportes",
+#         "Obras": "Desenvolvimento Urbano e Obras",
+#         "SEPLAMA": "Planejamento e Meio Ambiente",
+#         "Rural":"Desenvolvimento Rural",
+#         "Social": "Desenvolvimento Social",
+
+#     }
+    
+#     # Busca o nome limpo no dicionário. Se não achar, usa o próprio texto digitado na URL
+#     secretaria_nome = nomes_formatados.get(secretaria_nome, secretaria_nome)
+    
+#     # Injeta dinamicamente a variável 'secretaria' no contexto do template padrão
+#     return templates.TemplateResponse(
+#         request=request,
+#         name="nova_reuniao.html",
+#         context={"secretaria_nome": secretaria_nome,
+#                  "data_atual": data_atual,
+#                  "hora_atual": datetime.now().strftime("%H:%M")
+#         }
+#     )
+
+#página de pauta livre
+@app.get("/pauta-livre",response_class=HTMLResponse)
+async def pauta_livre(request: Request):
+
+    data_atual = datetime.now().strftime("%d/%m/%Y")
+
+    return templates.TemplateResponse(
+        request=request,
+        name="pauta_livre.html",
+        context={
+            "data_atual": data_atual,
+            "hora_atual": datetime.now().strftime("%H:%M")
+        }
+    )
+
+#USERs - E-mail
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return user_with_id
+
+@app.get("/users/", response_model=UserList)
+def read_users():
+    return {'users': database}
+
+@app.put("/users/{user_id}", response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="Usuário não encontrado"
+            )
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="Usuário não encontrado"
+            )
+    del database[user_id - 1]
+    return {"message": "Usuário deletado com sucesso"}
+
+
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,5 +1,11 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String#, Mapped, mapped_as_dataclass, mapped_column, registry
+# from sqlalchemy import func
+from datetime import datetime
 from database import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+#table_registry = registry()
 
 class Secretaria(Base):
     __tablename__ = "secretarias"
@@ -9,3 +15,17 @@ class Secretaria(Base):
     descricao = Column(String, index=True)
     def __repr__(self):
         return f"<Secretaria(nome='{self.nome}', descricao='{self.descricao}')>"
+
+
+# # @mapped_as_dataclass(table_registry)
+# # class User:
+# #     __tablename__ = 'users'
+
+# #     id: Mapped[int] = mapped_column(init=False, primary_key=True)
+# #     username: Mapped[str] = mapped_column(unique=True)
+# #     password: Mapped[str]
+# #     email: Mapped[str] = mapped_column(unique=True)
+# #     created_at: Mapped[datetime] = mapped_column(
+# #         init=False, server_default=func.now()
+# #     )
+
